de513baedbdd120b108e2d61c239f871e89f2c1c
MiradoAndy <miradoandy14@gmail.com>
2025-07-14 14:23:30 +0300

    .

--- inc/function.php
+++ inc/function.php
@@ -67,12 +67,12 @@ function liste_categorie()
     }
 }
 
-function filtre_objet_par_categorie($where)
+function filtre_objet_par_categorie($where = "")
 {
     $query = "SELECT o.nom_objet, o.id_objet, c.nom_categorie, e.date_retour
               FROM objet o
               JOIN categorie_objet c ON o.id_categorie = c.id_categorie
-              LEFT JOIN emprunt e ON o.id_objet = e.id_objet AND e.date_retour IS NULL
+              LEFT JOIN emprunt e ON o.id_objet = e.id_objet
               $where";
     return mysqli_query(bdconnect(), $query);
 }
